edc777513da2b21a35dbe3bfa7db041e8f93efa7
Siumeli <sameli.aro@edu.bc.fi>
2025-01-28 13:21:09 +0200

    Nuolia_kaupan tehtävä luotu

---
+++ Nuolia_kaupan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nuolia_kaupan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
