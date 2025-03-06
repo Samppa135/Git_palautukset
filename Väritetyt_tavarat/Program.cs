1c2d2781be1d7a2e87b17aa39b01849470d35012
Siumeli <sameli.aro@edu.bc.fi>
2025-03-06 09:55:33 +0200

    Väritetyt_tavarat luotu
    Projekti luotu valmiiksi

---
+++ Väritetyt_tavarat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Väritetyt_tavarat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
