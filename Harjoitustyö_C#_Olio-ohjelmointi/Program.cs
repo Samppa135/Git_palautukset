1466337420a3fd4a9c91be8e288a2904d15ad54c
Siumeli <sameli.aro@edu.bc.fi>
2025-03-06 10:03:58 +0200

    Harjoitustyö_C#_Olio-ohjelmointi luotu
    Projekti luotu valmiiksi

---
+++ Harjoitustyö_C#_Olio-ohjelmointi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Harjoitustyö_C__Olio_ohjelmointi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
